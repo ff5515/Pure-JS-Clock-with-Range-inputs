47997d0ffc1e014eacddec1def4b223ac2258124
VatcheGh <gharibyanv@gmail.com>
2020-10-26 18:27:43 +0400

    added date with animation

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let page = document.querySelector('.container');
+let today = document.querySelector('.today-div');
+
 let watch = document.querySelector('.outer-circle');
 let innerCircle = document.querySelector('.inner-circle')
 let clock = document.querySelector('.clock');
@@ -12,6 +14,31 @@ let handlerUD = document.querySelector('.handler-ud'); // UD = up down
 let colorChanger = document.querySelector('.color-changer');
 let scaleChanger = document.querySelector('.scale');
 
+let date = new Date().toLocaleDateString().split(' ').toString();
+//date into separate characters
+for (i=0; i < date.length; i++) {
+  today.innerHTML += `<span class='today'>` + date[i] + `</span>`
+}
+
+//date appearing animation
+function showDate () {
+  let char = 0;
+  function showDateClick() {
+    let todayChar = today.querySelectorAll('.today')[char];
+      todayChar.classList.toggle('today-appear');
+      char++;
+      if (char === date.length) {
+        complete();
+        return;
+      }
+    }
+    let timer = setInterval(showDateClick, 50); 
+    function complete() {
+      clearInterval(timer);
+      timer = null;
+    }
+  }
+
 //clock
 let currentTime = () => {
   let time = (new Date()).toLocaleTimeString().split('/:');
@@ -20,13 +47,13 @@ let currentTime = () => {
 }
 currentTime()
 
-//up and down 
+//up and down handler
 handlerLR.addEventListener('input', (e) => {
   let val = e.target.value;
   posX.style.transform = `translateX(${val}px)`;
 } )
 
-//left and right 
+//left and right handler
 handlerUD.addEventListener('input', (e) => {
   let val = e.target.value;
   posY.style.transform = `translateY(-${val}px)`
